69c2ad784c7bfe0e600e9bb1f2881b6c9fc8878e
otechservices <otechservices24@gmail.com>
2025-09-03 22:18:55 +0100

    Correction du système de routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TrainingComponent } from './components/training/training.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TrainingComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TrainingComponent } from './components/training/training.component';
+import { SimulatorComponent } from './components/simulator/simulator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'training', component: TrainingComponent },
+  { path: 'simulator', component: SimulatorComponent },
+  { path: '**', redirectTo: '' }
+];
