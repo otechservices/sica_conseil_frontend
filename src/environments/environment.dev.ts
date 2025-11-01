7999585bc5a11cb6634dcf77d702605809195ad7
Ornice <ornihouss1@gmail.com>
2025-11-01 23:20:36 +0100

    fix data

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_SCHEME: 'https',
-  API_DOMAIN: 'sicaconseil.otech-services.bj/api',
-  API_FILE: 'sicaconseil.otech-services.bj',
+  API_DOMAIN: 'sicaconseil-api.otech-services.bj/api',
+  API_FILE: 'sicaconseil-api.otech-services.bj',
   API_VERSION: 'v1'
 };
