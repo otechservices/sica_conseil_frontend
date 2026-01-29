67fb4b3b3d78082fe7b935be56e0b0dbab2febad
Ornice <ornihouss1@gmail.com>
2026-01-29 18:20:55 +0100

    fix catégorie inscription

--- src/app/core/utils/config-service.ts
+++ src/app/core/utils/config-service.ts
@@ -1,5 +1,5 @@
 import {  HttpHeaders } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 export const ConfigService: any = {
   apiVersion: environment.API_VERSION,
